b525f24fa6d64148d052073072a91469091e58a8
James <james.baster@opendataservices.coop>
2020-01-28 17:24:09 +0000

    Move all URLs to root, and change to URL tags in templates

--- foi_search/urls.py
+++ foi_search/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/', include('foi_search_app.urls')),
+    path('', include('foi_search_app.urls')),
 ]
