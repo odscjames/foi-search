90038a6e2a2cea86737699a0aca23ce88637a518
James <james.baster@opendataservices.coop>
2020-01-28 15:53:54 +0000

    Add Search Views

--- foi_search_app/views.py
+++ foi_search_app/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from elasticsearch import Elasticsearch
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'foi_search_app/index.html', {})
+
+
+def search(request):
+
+    search_query = request.GET.get('search')
+
+    elastic_query = {
+        "query": {
+            "simple_query_string": {
+                "query": search_query,
+                "fields": ["question"],
+                "default_operator": "and"
+            }
+        }
+    }
+
+    es = Elasticsearch()
+    res = es.search(index="foisearch", body=elastic_query)
+
+    context = {
+        'search_query': search_query,
+        'results': []
+    }
+    for hit in res['hits']['hits']:
+        context['results'].append(hit['_source'])
+
+
+    return render(request, 'foi_search_app/search.html', context)
