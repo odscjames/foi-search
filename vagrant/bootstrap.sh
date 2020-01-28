84e79a69547e97880b74e13912f78e645505eec9
James <james.baster@opendataservices.coop>
2020-01-28 14:46:28 +0000

    Start

---
+++ vagrant/bootstrap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+apt-get update
+
+DEBIAN_FRONTEND=noninteractive apt-get install -y python3 python3-pip apt-transport-https virtualenv
+
+wget -O - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
+
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
+
+apt-get update
+
+DEBIAN_FRONTEND=noninteractive apt-get install -y elasticsearch
+
+/bin/systemctl daemon-reload
+/bin/systemctl enable elasticsearch.service
